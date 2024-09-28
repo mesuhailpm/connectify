d033c8ee350b44864cda3343fbda0d31da56ac6d
suhailpm <tosuhailpm@gmail.com>
2024-09-28 16:53:39 +0530

    Server starting point --index.js renamed to server.js --nodemon for dev --ernvironment variable loading with the help of dotenv

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const chatSocket = require('./sockets/chatSocket');
+const http = require('http');
+const cors = require('cors')
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors())
+
+// MongoDB connection
+connectDB();
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/chats', require('./routes/chatRoutes'));
+
+// WebSocket setup
+chatSocket(server);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
