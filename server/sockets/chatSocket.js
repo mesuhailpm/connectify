f374f6e5169860b5125564d13c0b681736dbf975
suhailpm <tosuhailpm@gmail.com>
2024-11-16 17:40:38 +0530

    Add chat socket functionality with message handling and transaction management
    - Implemented `chatSocket` function to handle WebSocket connections using `socket.io`.
    - Added message handling with `sendMessage` event, creating and saving `Message` documents.
    - Utilized Mongoose transactions to ensure atomic operations when saving messages and updating chats.
    - Implemented error handling and transaction rollback in case of failures.
    - Emitted `sendMessageSuccess` and `sendMessageFailure` events based on the outcome of message processing.
    - Logged user connection and disconnection events for better traceability.

---
+++ server/sockets/chatSocket.js
@@ -0,0 +1,59 @@
+const { Server } = require("socket.io");
+const Message = require("../models/Message");
+const Chat = require("../models/Chat");
+const { default: mongoose } = require("mongoose");
+
+const chatSocket = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    // Handle incoming messages
+    socket.on("sendMessage", async ({ chatId, messageText, userId, _id }) => {
+      const newMessage = new Message({
+        chat: chatId,
+        sender: userId,
+        content: messageText,
+        status:'sent',
+      });
+      try {
+
+        const mongooseSession = await mongoose.startSession();
+        mongooseSession.startTransaction()
+        try {
+          await newMessage.save({ session: mongooseSession });
+
+          await Chat.findByIdAndUpdate(
+            chatId,
+            { lastMessage: newMessage._id },
+            { session: mongooseSession }
+          );
+          await mongooseSession.commitTransaction();
+        } catch (error) {
+          await mongooseSession.abortTransaction();
+          throw Error('Error saving the message on backend...'+error.message)
+
+        } finally {
+          await mongooseSession.endSession();
+        }
+
+        // Broadcast to specific chat room or user
+        io.emit("sendMessageSuccess", newMessage);
+      } catch (error) {
+        io.emit("sendMessageFailure", newMessage._id);
+        console.error("Error sending message:", error);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+  });
+};
+
+module.exports = chatSocket;
