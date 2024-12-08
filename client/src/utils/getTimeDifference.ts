2c1451927f65439f7620303308c03b0d1b18397a
suhailpm <tosuhailpm@gmail.com>
2024-12-08 07:40:38 +0530

    Add getTimeAgo function to format and display last seen time

---
+++ client/src/utils/getTimeDifference.ts
@@ -0,0 +1,86 @@
+const MONTH_NAMES = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  const WEEKDAY_NAMES = [
+    'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+  ];
+  
+
+  function getFormattedDate(date: Date, prefomattedDate: false | string = false, hideYear = false) {
+    const day = date.getDate();
+    const month = MONTH_NAMES[date.getMonth()];
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    let minutes: string | number = date.getMinutes();
+  
+    if (minutes < 10) {
+      // Adding leading zero to minutes
+      minutes = `0${ minutes }`;
+    }
+  
+    if (prefomattedDate) {
+      // Today at 10:20
+      // Yesterday at 10:20
+      return `${ prefomattedDate } at ${ hours }:${ minutes }`;
+    }
+  
+    if (hideYear) {
+      // 10. January at 10:20
+      return `${ day }. ${ month } at ${ hours }:${ minutes }`;
+    }
+  
+    // 10. January 2017. at 10:20
+    return `${ day }. ${ month } ${ year }. at ${ hours }:${ minutes }`;
+  }
+  
+  
+
+  // --- Main function
+function getTimeAgo(dateParam: Date) {
+    if (!dateParam) {
+      return null;
+    }
+  
+    const date = typeof dateParam === 'object' ? dateParam : new Date(dateParam);
+    const DAY_IN_MS = 86400000; // 24 * 60 * 60 * 1000
+    const today = new Date();
+    const yesterday = new Date(today.getTime() - DAY_IN_MS);
+    const seconds = Math.round((today.getTime() - date.getTime()) / 1000);
+    const minutes = Math.round(seconds / 60);
+    const isToday = today.toDateString() === date.toDateString();
+    const isYesterday = yesterday.toDateString() === date.toDateString();
+    const daysAgo = Math.floor((today.getTime() - date.getTime()) / DAY_IN_MS);
+
+    const isLastWeek = daysAgo > 1 && daysAgo <= 7;
+    const isThisYear = today.getFullYear() === date.getFullYear();
+    
+  
+    if (seconds < 5) {
+      return 'very recently';
+    } else if (seconds < 60) {
+      return `${ seconds } seconds ago`;
+    } else if (seconds < 90) {
+      return 'about a minute ago';
+    } else if (minutes < 60) {
+      return `${ minutes } minutes ago`;
+    } else if (isToday) {
+      return getFormattedDate(date, 'Today'); // Today at 10:20
+    } else if (isYesterday) {
+      return getFormattedDate(date, 'Yesterday'); // Yesterday at 10:20
+    } else if (isLastWeek) {
+      const weekday = WEEKDAY_NAMES[date.getDay()];
+      return `${weekday} at ${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`;
+    }
+    
+    else if (isThisYear) {
+      return getFormattedDate(date, false, true); // 10. January at 10:20
+    }
+  
+    return getFormattedDate(date); // 10. January 2017. at 10:20
+  }
+
+
+  export default getTimeAgo
+  
