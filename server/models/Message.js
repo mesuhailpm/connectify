418e7c5df35f1b96a5db583a79b6ebabc9df2950
suhailpm <tosuhailpm@gmail.com>
2025-02-16 23:17:35 +0530

    used mongoose post hook to update the lastMessage for the correspponding chat to the last updated message also set message status to read if message.target is same as message.readBy

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Chat = require('./Chat');
 const { Schema } = mongoose;
 
 const messageSchema = new Schema({
@@ -13,6 +14,20 @@ const messageSchema = new Schema({
     default: 'sent',
   }
 }, { timestamps: true });
+
+
+messageSchema.post('findOneAndUpdate', async function (doc){
+  try {
+    await Chat.findOneAndUpdate({_id: doc.chat}, {lastMessage: doc._id}, {new: true} )
+    if (doc.target.length === doc.readBy.length) {
+      doc.status = 'read'
+    }
+    await doc.save()
+    console.log('chat is updated its last message')
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 const Message = mongoose.model('Message', messageSchema);
 module.exports = Message;
