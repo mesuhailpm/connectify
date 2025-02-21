54306329bbe1aac2d369cac5067093de4a5b3e3a
suhailpm <tosuhailpm@gmail.com>
2025-02-21 12:53:24 +0530

    basix setup for PWA

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 import { createStoreHook, Provider } from "react-redux";
 import store from "./reducers";
 
@@ -15,6 +16,8 @@ root.render(
     </Provider>
   </BrowserRouter>
 );
+
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
